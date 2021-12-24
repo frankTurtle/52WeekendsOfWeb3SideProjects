6cda08cac6d8dcdef999e8b3b90856bbef7dacd5
frankTurtle.crypto <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-12-23 21:57:08 -0500

    Feature/full stack (#3)
    * Initial project setup
    
    * Updated the app.js with new contract address and renamed script so to deploy it
    
    * Updated hardhat config with infura URL and private key
    
    * Created basic Token
    
    * Updated deploy script with the new token
    
    * Updated UI to be able to get the token balance
    
    * Added openzeppelin for erc20 spec on the token
    
    * Updated Readme
    
    Co-authored-by: frankTurtle.crypto <frank@xn--qo8h.kz>

---
+++ Weekend 3 - Full Stack/contracts/Token.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "52 Weekend Projects";
+    string public symbol = "52WOSP";
+    uint256 public totalSupply = 52;
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
